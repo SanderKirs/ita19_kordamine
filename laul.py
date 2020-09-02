c59932315bae1a91ed4eb16979e1769728bc9c6b
SanderKirs <69902319+SanderKirs@users.noreply.github.com>
2020-09-02 10:59:18 +0300

    Delete laul.py

--- laul.py
+++
@@ -1,8 +0,0 @@
-class Laul:
-
-    def __init__(self, pealkiri, laulja):
-        self.pealkiri = pealkiri
-        self.laulja = laulja
-
-laul1 = Laul ("Für oksana", "Nublu")
-print (laul1.pealkiri,"-",laul1.laulja)
