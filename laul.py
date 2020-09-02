99fecc51c58d70c982733a46cd0f68a48f2f0a93
Sander Kirs <sander.kirs@khk.ee>
2020-09-02 10:53:26 +0300

    Laulu klassi näidis koos ühe objektiga.

---
+++ laul.py
@@ -0,0 +1,8 @@
+class Laul:
+
+    def __init__(self, pealkiri, laulja):
+        self.pealkiri = pealkiri
+        self.laulja = laulja
+
+laul1 = Laul ("Für oksana", "Nublu")
+print (laul1.pealkiri,"-",laul1.laulja)
