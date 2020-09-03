11a72c86e58c26f732885b2b236eef3b4d771467
Sander Kirs <sander.kirs@khk.ee>
2020-09-03 15:16:16 +0300

    On koostatud Laulja klass ja testitud antud klassi objekti loomist ning koostöö teiste objektidega

--- albumi_fail.py
+++ albumi_fail.py
@@ -1,10 +1,13 @@
 from laulu_fail import Laul
 
 class Album:
-    def __init__(self, nimetus, aasta, artist):
-        self.nimetus = nimetus
+    def __init__(self, pealkiri, aasta, artist):
+        self.pealkiri = pealkiri
         self.aasta = aasta
         self.artist = artist
         self.laulud = []
+
+    def lisa_laul(self,laul):
+        self.laulud.append(laul)
 
 
--- menüü.py
+++ menüü.py
@@ -1,18 +1,41 @@
 from laulu_fail import Laul
 from albumi_fail import Album
+from laulja import Laulja
 #Testime objekti loomist
-laul1 = Laul ("Für oksana", "Nublu")
+laul1 = Laul ("laul 1", "Nublu")
 print (laul1.pealkiri,"-",laul1.laulja)
 
 laul2 = Laul ("laul 2", "Nublu")
 print (laul2.pealkiri,"-",laul2.laulja)
 
+laul3 = Laul ("laul 3", "Nublu")
+print (laul3.pealkiri,"-",laul3.laulja)
+
+laul4 = Laul ("laul 4", "Nublu")
+print (laul3.pealkiri,"-",laul3.laulja)
 #Testime albumi loomist
-album = Album ("Uus album", "2019", "Nublu")
+album1 = Album ("Uus album", "2019", "Nublu")
+
+album2 = Album ("Vana album", "2018", "Nublu")
 
 #Lisame laulud albumisse
-album.laulud.append(laul1)
-album.laulud.append(laul2)
+album1.lisa_laul(laul1)
+album1.lisa_laul(laul2)
+album2.lisa_laul(laul3)
+album2.lisa_laul(laul4)
 
-for laul in album.laulud:
+for laul in album1.laulud:
     print (laul.laulja, "-",laul.pealkiri)
+for laul in album2.laulud:
+    print (laul.laulja, "-",laul.pealkiri)
+
+#Testime Laulja klassi
+laulja = Laulja ("Nublu")
+laulja.lisa_album(album1)
+laulja.lisa_album(album2)
+
+#Testime albumite sisu
+for album in laulja.albumid:
+    print(album.pealkiri)
+    for laul in album.laulud:
+        print(laul.laulja, laul.pealkiri)
