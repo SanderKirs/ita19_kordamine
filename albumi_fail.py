cc0fbbd6aab3fbaa668e4bd6a77b4eac1096ef47
Sander Kirs <sander.kirs@khk.ee>
2020-09-03 14:25:15 +0300

    On koostatud Albumi klass ja testitud antud klassi objekti loomist ning sisu vaatamist

--- albumi_fail.py
+++ albumi_fail.py
@@ -1,10 +1,10 @@
 from laulu_fail import Laul
 
 class Album:
-    def __init__(self, nimetus, aasta, artist, laulud):
+    def __init__(self, nimetus, aasta, artist):
         self.nimetus = nimetus
         self.aasta = aasta
         self.artist = artist
-        self.laulud = laulud
-album1 = Album("Thrash Zone", "1989", "D.R.I", Laul("Thrasher", "D.R.I"))
-print(album1.nimetus,album1.aasta,album1.artist,album1.laulud)
+        self.laulud = []
+
+
--- menüü.py
+++ menüü.py
@@ -1,4 +1,18 @@
 from laulu_fail import Laul
-
+from albumi_fail import Album
+#Testime objekti loomist
 laul1 = Laul ("Für oksana", "Nublu")
 print (laul1.pealkiri,"-",laul1.laulja)
+
+laul2 = Laul ("laul 2", "Nublu")
+print (laul2.pealkiri,"-",laul2.laulja)
+
+#Testime albumi loomist
+album = Album ("Uus album", "2019", "Nublu")
+
+#Lisame laulud albumisse
+album.laulud.append(laul1)
+album.laulud.append(laul2)
+
+for laul in album.laulud:
+    print (laul.laulja, "-",laul.pealkiri)
