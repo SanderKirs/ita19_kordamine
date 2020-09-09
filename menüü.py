04b31ec7ce2aeb417e1fbb359e33b65e759e9f4b
Sander Kirs <sander.kirs@khk.ee>
2020-09-09 09:56:52 +0300

    On lisatud albumite failist info lugemine

--- menüü.py
+++ menüü.py
@@ -1,6 +1,8 @@
+#Importime teistest failidest vajalikud klassid
 from laulu_fail import Laul
 from albumi_fail import Album
 from laulja import Laulja
+
 #Testime objekti loomist
 laul1 = Laul ("laul 1", "Nublu")
 print (laul1.pealkiri,"-",laul1.laulja)
@@ -12,7 +14,8 @@ laul3 = Laul ("laul 3", "Nublu")
 print (laul3.pealkiri,"-",laul3.laulja)
 
 laul4 = Laul ("laul 4", "Nublu")
-print (laul3.pealkiri,"-",laul3.laulja)
+print (laul4.pealkiri,"-",laul4.laulja)
+
 #Testime albumi loomist
 album1 = Album ("Uus album", "2019", "Nublu")
 
@@ -24,12 +27,13 @@ album1.lisa_laul(laul2)
 album2.lisa_laul(laul3)
 album2.lisa_laul(laul4)
 
+#Testime for tsükli abil laulu printimist albumist
 for laul in album1.laulud:
     print (laul.laulja, "-",laul.pealkiri)
 for laul in album2.laulud:
     print (laul.laulja, "-",laul.pealkiri)
 
-#Testime Laulja klassi
+#Testime Laulja klassi ja funktsiooni lisa_album
 laulja = Laulja ("Nublu")
 laulja.lisa_album(album1)
 laulja.lisa_album(album2)
@@ -39,3 +43,10 @@ for album in laulja.albumid:
     print(album.pealkiri)
     for laul in album.laulud:
         print(laul.laulja, laul.pealkiri)
+
+#Loeme albumite failist laulud,albumid ja nende aastad
+järk = []
+with open("albumid", "rt") as f:
+    for line in f:
+        järk.append(line)
+        print(järk[-1])
